696380e5ac28d519e80b0b9bd4ce10c81a0ae5ee
KayGV25 <khuongvudang25@gmail.com>
2023-11-18 20:35:10 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let strNum  = '';
 let calculated = 0;
 let temp = 0;
-const allow = ['0','1','2','3','4','5','6','7','8','9'];
+const allow = ['0','1','2','3','4','5','6','7','8','9','.'];
 operator = '';
 
 function ShowNumIn(strInNum){
